0f869b0c0faa48d2205719bee5bf273007eee4e8
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2023-01-21 23:23:03 +0100

    Compare two drawn cards and select winning player   - The computeValue function converts the card value from a string to a     Number

--- js/main.js
+++ js/main.js
@@ -18,8 +18,36 @@ function drawTwoCards() {
     fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`)
         .then(res => res.json())
         .then(result => {
+            let resultMessage = ''
             document.getElementById('playerOneCard').src = result.cards[0].image
             document.getElementById('playerTwoCard').src = result.cards[1].image
+
+            let playerOneCardValue = computeValue(result.cards[0].value)
+            let playerTwoCardValue = computeValue(result.cards[1].value)
+
+            if (playerOneCardValue > playerTwoCardValue) {
+                resultMessage = 'Player 1 wins!'
+            } else if (playerOneCardValue < playerTwoCardValue) {
+                resultMessage = 'Player Two Wins!'
+            } else {
+                resultMessage = 'I declare a car war!'
+            }
+
+            document.getElementById('result').innerText = resultMessage
             console.log(result.remaining)
         })
+}
+
+function computeValue(value) {
+    if (value == 'ACE') {
+        return 14
+    } else if (value == 'KING') {
+        return 13
+    } else if (value == 'QUEEN') {
+        return 12
+    } else if (value == 'JACK') {
+        return 11
+    } else {
+        return Number(value)
+    }
 }
