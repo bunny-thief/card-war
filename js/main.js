94ac9d39c1ae50cb5d5c5eb04a1a6ec9e109191c
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2023-01-21 22:33:28 +0100

    Deck is shuffled and 'deck_id' is captured

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+// document.getElementById('shuffleDeck').addEventListener('click')
+let deck_id = ''
 
+if (!localStorage.getItem('deck_id')) {
+
+    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(res => res.json())
+        .then(result => {
+            deck_id = result.deck_id
+            localStorage.setItem('deck_id', deck_id)
+            console.log(deck_id)
+        })
+}
+
+console.log(deck_id)
+console.log(deck_id)
