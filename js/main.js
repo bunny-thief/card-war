ec0dcece498d0064eb824f9ac180e8d2ee36f733
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2023-01-24 22:03:34 +0100

    Drawn cards are moved to winning player's pile
      - Drawn card objects are held in 'cardsDrawn' array
      - Card codes are then pushed to 'cardsForPile' array
      - The card codes are then concatenated and stored in 'cards'
      - Then 'moveCardsToPile' function then moves drawn cards to the pile
        of the player that wins the hand
      - Added documentation to 'drawTwoCards' function
    
      Closes #1

--- js/main.js
+++ js/main.js
@@ -14,29 +14,63 @@ if (!localStorage.getItem('deck_id') || localStorage.getItem('deck_id') == 'null
     console.log(`Deck loaded from localStorage: ${deck_id}`)
 }
 
+// draws two cards from shuffled deck
 function drawTwoCards() {
     fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`)
         .then(res => res.json())
         .then(result => {
             let resultMessage = ''
+
+
             document.getElementById('playerOneCard').src = result.cards[0].image
             document.getElementById('playerTwoCard').src = result.cards[1].image
 
             let playerOneCardValue = computeValue(result.cards[0].value)
             let playerTwoCardValue = computeValue(result.cards[1].value)
 
+            // holds card objects drawn
+            let cardsDrawn = result.cards
+
+            // will hold array of card codes
+            let cardsForPile = []
+
+            // inserts card codes into 'cardForPile' array
+            for (let card of cardsDrawn) {
+                // console.log(card.code)
+                cardsForPile.push(card.code)
+            }
+            console.log(`cardsForPile: ${cardsForPile}`)
+
+            // creates string of card codes from 'cardsForPile' separated by commas
+            let cards = cardsForPile.join(",")
+
             if (playerOneCardValue > playerTwoCardValue) {
                 resultMessage = 'Player 1 wins!'
+                moveCardsToPile('playerOnePile', cards)
+
             } else if (playerOneCardValue < playerTwoCardValue) {
                 resultMessage = 'Player Two Wins!'
+                moveCardsToPile('playerTwoPile', cards)
+
             } else {
-                resultMessage = 'I declare a car war!'
+                resultMessage = 'I declare a card war!'
             }
 
             document.getElementById('result').innerText = resultMessage
-            console.log(result.remaining)
+            console.log(`Reamining cards: ${result.remaining}`)
         })
 }
+
+
+function moveCardsToPile(pileName, cards) {
+    fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/pile/${pileName}/add/?cards=${cards}`)
+        .then(res => res.json())
+        .then(result => {
+            console.log(`${cards} added to ${pileName}`)
+        })
+        .catch(err => console.log(`error: ${err}`))
+}
+
 
 function computeValue(value) {
     if (value == 'ACE') {
