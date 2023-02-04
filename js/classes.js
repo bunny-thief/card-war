d7445136e0aab340ba5a8fe466fb85d2d4ce6887
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2023-02-04 11:52:22 +0100

    Make 'Deck' functions async
    Closes #6

--- js/classes.js
+++ js/classes.js
@@ -8,56 +8,69 @@ export class Deck {
         return this._deckId
     }
 
-    createDeck() {
-        // get a new deck id if a deck id doesn't exist or if has a value of 'null'
-        if (!localStorage.getItem('deck_id') || localStorage.getItem('deck_id') == 'null') {
-            fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-                .then(res => res.json())
-                .then(result => {
-                    localStorage.setItem('deck_id', result.deck_id)
-                    this._deckId = localStorage.getItem('deck_id')
-                    // this._remainingCards = 52
-                    console.log(`New deck added to localStorage: ${this._deckId}`)
-                })
-        } else {
-            // get a deck id from localstorage
-            this._deckId = localStorage.getItem('deck_id')
-            console.log(`Deck loaded from localStorage: ${this._deckId}`)
+    async createDeck() {
+        try {
+            // get a new deck id if a deck id doesn't exist or if has a value of 'null'
+            if (!localStorage.getItem('deck_id') || localStorage.getItem('deck_id') == 'null') {
+                const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+
+                if (!response.ok) {
+                    throw new Error(`HTTP Error: ${response.status}`)
+                }
+                const result = await response.json()
+
+                localStorage.setItem('deck_id', result.deck_id)
+                this._deckId = localStorage.getItem('deck_id')
+
+            } else {
+                // get a deck id from localstorage
+                this._deckId = localStorage.getItem('deck_id')
+                console.log(`Deck loaded from localStorage: ${this._deckId}`)
+            }
+        }
+        catch (error) {
+            console.log(`Could not create new deck: ${error}`)
         }
     }
 
-    distributeCards(numberOfCards, p1PileName, p2PileName) {
-        fetch(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=${numberOfCards}`)
-            .then(res => res.json())
-            .then(result => {
-                console.log(`distributing cards...`)
+    async distributeCards(numberOfCards, p1PileName, p2PileName) {
+        try {
+            const response = await fetch(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=${numberOfCards}`);
 
-                // holds card objects drawn
-                let cardsDrawn = result.cards
+            if (!response.ok) {
+                throw new Error(`HTTP Error: ${response.status}`)
+            }
 
-                // will hold array of card codes
-                let cardsForPlayerOnePile = []
-                let cardsForPlayerTwoPile = []
+            const result = await response.json()
 
+            console.log(`distributing cards...`)
 
-                // 'cardsDrawn' will sometimes be undefined because its assignment doesn't always
-                // occur before it is accessed in the for loop below
+            // holds card objects drawn
+            const cardsDrawn = result.cards
 
-                // insert card codes into array
-                for (let i = 0; i < cardsDrawn.length - 1; i += 2) {
-                    cardsForPlayerOnePile.push(cardsDrawn[i].code)
-                    cardsForPlayerTwoPile.push(cardsDrawn[i + 1].code)
-                }
+            // will hold array of card codes
+            const cardsForPlayerOnePile = []
+            const cardsForPlayerTwoPile = []
 
-                // creates string of card codes separated by commas
-                let cardsToMovePlayerOne = cardsForPlayerOnePile.join(",")
-                let cardsToMovePlayerTwo = cardsForPlayerTwoPile.join(",")
+            // insert card codes into arrays
+            for (let i = 0; i < cardsDrawn.length - 1; i += 2) {
+                cardsForPlayerOnePile.push(cardsDrawn[i].code)
+                cardsForPlayerTwoPile.push(cardsDrawn[i + 1].code)
+            }
 
-                this.players[0].addCardsToPile(p1PileName, cardsToMovePlayerOne)
-                this.players[1].addCardsToPile(p2PileName, cardsToMovePlayerTwo)
-            })
-            .catch(err => console.log(`error: ${err}: ${err.lineNumber}`))
+            // creates string of card codes separated by commas
+            let cardsToMovePlayerOne = cardsForPlayerOnePile.join(",")
+            let cardsToMovePlayerTwo = cardsForPlayerTwoPile.join(",")
+
+            this.players[0].addCardsToPile(p1PileName, cardsToMovePlayerOne)
+            this.players[1].addCardsToPile(p2PileName, cardsToMovePlayerTwo)
+        }
+
+        catch (error) {
+            console.log(`Could not distribute cards: ${error}`)
+        }
     }
+
 }
 
 
